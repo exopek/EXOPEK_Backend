26662183d8def91327b269fa75b5c4b97a991444
Jan Sugint <jan@exopek.de>
2023-08-31 08:45:05 +0200

    map each properties for exerciseresponse

--- Application/Dtos/MappingProfile.cs
+++ Application/Dtos/MappingProfile.cs
@@ -14,8 +14,46 @@ public class MappingProfile : Profile
         CreateMap<Workout, WorkoutSingleResponse>()
             .ForMember(dest =>
                 dest.Exercises, opt =>
-                opt.MapFrom(src => src.WorkoutExercises.Select(we => we.Exercise)));
-            
+                opt.MapFrom(src => src.WorkoutExercises));
+        CreateMap<WorkoutExercise ,ExerciseResponse>()
+            .ForMember(dest =>
+                dest.Id , opt =>
+                    opt.MapFrom(src => src.Exercise.Id))
+            .ForMember(dest =>
+                dest.Name , opt =>
+                opt.MapFrom(src => src.Exercise.Name))
+            .ForMember(dest =>
+                dest.StageType , opt =>
+                opt.MapFrom(src => src.StageType))
+            .ForMember(dest =>
+                dest.VideoUrl , opt =>
+                opt.MapFrom(src => src.Exercise.VideoUrl))
+            .ForMember(dest =>
+                dest.Category , opt =>
+                opt.MapFrom(src => src.Exercise.Category))
+            .ForMember(dest =>
+                dest.Description , opt =>
+                opt.MapFrom(src => src.Exercise.Description))
+            .ForMember(dest =>
+                dest.PreviewImageUrl , opt =>
+                opt.MapFrom(src => src.Exercise.PreviewImageUrl))
+            .ForMember(dest =>
+                dest.StageRound , opt =>
+                opt.MapFrom(src => src.StageRound))
+            .ForMember(dest =>
+                dest.StageOrder , opt =>
+                opt.MapFrom(src => src.StageOrder))
+            .ForMember(dest =>
+                dest.Duration , opt =>
+                opt.MapFrom(src => src.Duration))
+            .ForMember(dest =>
+                dest.Reps , opt =>
+                opt.MapFrom(src => src.Reps))
+            .ForMember(dest =>
+                dest.Difficulty , opt =>
+                opt.MapFrom(src => src.Exercise.Difficulty))
+            ;
+
         CreateMap<UserRegisterRequest, User>();
     }
 
